d7ffcb594a26d1298861b8373efa29f87eaf8f14
Bruno Pellizzetti <pelli.br@gmail.com>
2024-05-01 18:31:40 -0300

    updates

--- filefixer.py
+++ filefixer.py
@@ -8,29 +8,31 @@ def process_chat_files(filename):
         # Initialize the processed lines container
         processed_lines = []
         # Regular expression for lines starting with date time format
-        pattern = r'^\[\d{2}/\d{2}/\d{4}, \d{2}:\d{2p}:\d{2}\]'
-        last_line = ''
+        pattern = r'^\[\d{2}\/'
+
+    # for line in lines:
+        # Clean the invalid characters
+        # prin(line)
+        # processed_lines.append(line)
+        # line = line.replace('\u200e', '')
+
 
     for line in lines:
-        # Clean the invalid characters
-        line = line.replace('\u200e', '')
+        cleaned_line = line.strip()
         # If the line doesn't start with date time format, join with last line
-        if not re.match(pattern, line):
-            if last_line != '':
-                processed_lines[-1] = f'{last_line.strip()} {line}'
-                continue
-                
-        # If the line starts with the date time format, add to the processed lines 
-        processed_lines.append(line)
-        last_line = line
+        if not re.match(pattern, cleaned_line):
+            joiner = line[-1] + line
+            print(joiner)
+            processed_lines.append(joiner)
+        else:
+            processed_lines.append(line)
 
 
         
      # Write to a new file 
-    with open('chat2-----fixed.txt', 'w') as file:
+    with open('./whats/_chat_to_join_fixed.txt', 'w') as file:
         for line in processed_lines:
             file.write(line)
 
-    return 'fixed_file2.txt'
 
-process_chat_files('_chat.txt')
+process_chat_files('./whats/_chat_to_join.txt')
--- importer-functions.py
+++ importer-functions.py
@@ -2,8 +2,8 @@ import re
 import json
 import codecs
 from bs4 import BeautifulSoup
-from filefixer import process_chat_files
-import fitz
+#from filefixer import process_chat_files
+#import fitz
 
 
 def decode_unicode_escape(string_with_escapes):
@@ -16,7 +16,9 @@ def parse_chat_messages(text):
     messages = []
     unique_senders = {}
     for line_number, line in enumerate(text.split("\n")):
-        attachment = None
+        #attachment = None
+        #print(line)
+        messages.append({"attachment": line})
         match = message_pattern.match(line)
         if match:
             date, time, sender, message = match.groups()
@@ -30,42 +32,46 @@ def parse_chat_messages(text):
                 "time": time,
                 "sender": sender,
                 "sender_id": sender_id,
-                "message": message,
-                "attachment": attachment
+                "message": message
             })
     return messages
 
 def parse_chat_messages_attachments(dicttowork):
     attachment_pattern = re.compile(r'<anexado:\s([^<>]+)>')
     messages = dicttowork
+    #print(messages)
     for dict in dicttowork:
-        mensagem = dict.get('message')
-        attachment_match = attachment_pattern.match(mensagem)
-        if attachment_match:
-            dict['attachment'] = attachment_match.group(1)
+        mensagem = dict.get('attachment')
+        print(mensagem)
+        #attachment_match = attachment_pattern.match(mensagem)
+        #if attachment_match:
+        #    dict['attachment'] = attachment_match.group(1)
     return messages
 
-with open('./whats/_chat2.txt', mode='r', encoding='UTF-8') as file:
+with open('./whats/_chat.txt', mode='r', encoding='UTF-8') as file:
     text = decode_unicode_escape(file.read())
 
 
 
-objectToWork = parse_chat_messages(text)
-parsed = parse_chat_messages_attachments(objectToWork)
+object_to_work = parse_chat_messages(text)
+parsed_with_attachments = parse_chat_messages_attachments(object_to_work)
 
+#print(parsed_with_attachments)
 
 html = ""
 
-for message in parsed:
-    if message['attachment'] is not None and message['attachment'].endswith('.opus'):
-        print(message['attachment'])
-    if message['attachment'] is not None and message['attachment'].endswith('.pdf'):
-        print(message['attachment'])
-    if message['attachment'] is not None and message['attachment'].endswith('.jpg'):
-        print(message['attachment'])
-    if message['attachment'] is not None and message['attachment'].endswith('.vcf'):
-        print(message['attachment'])
-    
+def attachment_checker(messages):
+    for message in messages:
+        if message['attachment'] is not None and message['attachment'].endswith('.opus'):
+            print(message['attachment'])
+        if message['attachment'] is not None and message['attachment'].endswith('.pdf'):
+            print(message['attachment'])
+        if message['attachment'] is not None and message['attachment'].endswith('.jpg'):
+            print(message['attachment'])
+        if message['attachment'] is not None and message['attachment'].endswith('.vcf'):
+            print(message['attachment'])
+
+
 
 
 
