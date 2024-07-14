e6360f55cb8186d2848b23f00f130393dde67c7b
Bruno Pellizzetti <pelli.br@gmail.com>
2024-07-14 16:12:58 -0300

    append list a to b

--- projeto/find_whats_key_data.py
+++ projeto/find_whats_key_data.py
@@ -1,3 +1,12 @@
 def find_whats_key(data):
-    whats_item = next((item['whats'] for item in data if 'whats' in item), None)
-    return whats_item
+    for item in data:
+        if 'name' in item:
+            if 'Whats' in item['name']:
+                print("Found 'Whats' in name")
+                return item['name']
+            elif 'chat' in item['name']:
+                print("Found 'chat' in name")
+                return item['name']
+    print("Error: Neither 'whats' nor 'chat' were found in any item name")
+    raise ValueError("Neither 'whats' nor 'chat' were found in any item name")
+
