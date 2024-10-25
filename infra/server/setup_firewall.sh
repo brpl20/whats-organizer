2403214cf2bdac84cd4af5f9e17ae9b9640d1742
wh1t3h47 <tom.mharres@gmail.com>
2024-10-24 23:27:01 -0300

    fix(infra): Configuração manual de SSL/ Burlar plano pago Cloudflare :whale:

---
+++ infra/server/setup_firewall.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# Necessary workaround to provide ssl in more than one port using free
+# cloudflare plan this blocks all IPS outside cloudflare's network, it
+# prevents deanonimization via SSL fingreprint (Censys, Shodan...)
+
+CF_IPV4_URL="https://www.cloudflare.com/ips-v4"
+CF_IPV6_URL="https://www.cloudflare.com/ips-v6"
+
+CLOUDFLARE_IPS_FIREWALL="$(wget -qO- ${CF_IPV4_URL})\n$(wget -qO- ${CF_IPV6_URL})"
+
+export CLOUDFLARE_IPS_FIREWALL=$(printf "allow %s;\n" $CLOUDFLARE_IPS_FIREWALL)"\
+# allow 127.0.0.1;\
+# Docker's IP\
+# allow 172.17.0.0/16;\
+deny all;"
+echo ${CLOUDFLARE_IPS_FIREWALL}
