e1bb42a4033d4630b11b7c069ebeb130c10fde21
wh1t3h47 <tom.mharres@gmail.com>
2024-11-12 16:58:42 -0300

    chore: Melhorar mensagem websocket :wrench:

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ socketio = SocketIO(app,
 
 @socketio.on('connect')
 def handle_connect():
-    socketio.emit('Smessage', {'data': 'Connected'})
+    socketio.emit('Smessage', {'data': 'Enviando Arquivo...'})
 
 
 @app.route('/process', methods=['POST'])
@@ -63,25 +63,25 @@ def process_zip():
         # Save the uploaded file
         zip_path = os.path.join(final_work_folder, "uploaded.zip")
         file.save(zip_path)
-        socketio.emit('Smessage', {'data': 'Arquivo salvo com sucesso'})
+        socketio.emit('Smessage', {'data': 'Arquivo Salvo com Sucesso!'})
         
         # Process the zip file
         handle_zip_file(zip_path, final_work_folder)
-        socketio.emit('Smessage', {'data': 'Criando Pastas Profisórias'})
+        socketio.emit('Smessage', {'data': 'Criando Pastas Provisórias...'})
         
         # List objects in the directory
         file_object = list_files_in_directory(final_work_folder)
-        socketio.emit('Smessage', {'data': 'Listando Arquivos'})
+        socketio.emit('Smessage', {'data': 'Coletando Arquivos da Conversa...'})
         
         # Convert MP3 and transcribe
         transcriptions = convert_opus_to_mp3(final_work_folder)
-        socketio.emit('Smessage', {'data': 'Convertendo MP3 e Transcrevendo Áudios'})
+        socketio.emit('Smessage', {'data': 'Convertendo MP3 e Transcrevendo Áudios...'})
         
         # Transform PDF into Images 
         print("Converter PDF, transformar em imagens e links")
         bucket_name = 'tempfilesprocessing'
         pdf_img_links = process_pdf_folder(final_work_folder, bucket_name)
-        socketio.emit('Smessage', {'data': 'Trabalhando com PDFs e DOCXs'})
+        socketio.emit('Smessage', {'data': 'Trabalhando com PDFs e DOCXs...'})
 
         # Extract main conversation
         whats_main_file = find_whats_key(file_object)
@@ -89,7 +89,7 @@ def process_zip():
         
         # Extract device info
         dispositivo = extract_info_device(whats_main_folder_file)
-        socketio.emit('Smessage', {'data': 'Extraindo Informações do Dispositivo'})
+        socketio.emit('Smessage', {'data': 'Extraindo Informações do Dispositivo...'})
         
         # Fix files
         fixed_file = process_file_fixer(whats_main_folder_file, dispositivo)
@@ -97,17 +97,17 @@ def process_zip():
         # Extract info based on device type
         if dispositivo == "android":
             extracted_info = extract_info_android(fixed_file)
-            socketio.emit('Smessage', {'data': 'Dispositivo: Android'})
+            socketio.emit('Smessage', {'data': 'Android Detectado!'})
         elif dispositivo == "iphone":
             extracted_info = extract_info_iphone(fixed_file)
-            socketio.emit('Smessage', {'data': 'Dispositivo: Iphone'})
+            socketio.emit('Smessage', {'data': 'Iphone Detectado!'})
         else:
             return jsonify({"error": "Unknown device type"}), 400
         
         # Append files
         list_files = file_appending(extracted_info, transcriptions)
         lit_files_pdf = file_appending_pdf(extracted_info, pdf_img_links)
-        socketio.emit('Smessage', {'data': 'Organizando Mensagens'})
+        socketio.emit('Smessage', {'data': 'Organizando Mensagens...'})
 
         print(extracted_info)
         print(type(extracted_info))
@@ -134,6 +134,7 @@ def process_zip():
         else:
             print(f"Folder does not exist: {final_work_folder}")
     
+        socketio.emit('Smessage', {'data': 'Processamento Finalizado!'})
         return jsonify(result)
         
     return jsonify({"error": "Invalid file format"}), 400
